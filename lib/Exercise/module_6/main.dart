dfef7cc180d0b3149552a4e0bfa295599493e4cc
Md Ismail Hussain <ismailshuvo554@gmail.com>
2025-08-13 20:23:19 +0600

    module 6

--- lib/Exercise/module_6/main.dart
+++ lib/Exercise/module_6/main.dart
@@ -1,4 +1,7 @@
+import 'dart:ui_web';
+
 import 'package:demo_app/Exercise/module_6/Mobile.dart';
+import 'package:demo_app/Exercise/module_6/Tablet.dart';
 import 'package:demo_app/Exercise/module_6/shop.dart';
 
 void main(){
@@ -12,10 +15,62 @@ void main(){
   print(iphone13.getPrice.toStringAsFixed(2));
   print(iphone13.getStorage); //called by get method
   print(iphone13.deviceStorage()); // called by method
-  
+
+  Tablet ipad11Gen = Tablet(model: "iPad 11 Gen", year: "2022", price: 50000, screenSize: 1980);
   Shop gadgetShop = Shop('GnG', 'Mirpur, Dhaka');
+
   gadgetShop.addNewMobile(iphone13);
-  gadgetShop.sellAmobile("iPhone");
+  gadgetShop.addNewMobile(ipad11Gen);
+
+  gadgetShop.sellAmobile("iPad 11 Gen");
+
   print(gadgetShop.getMobileList);
+
+  Human student = Student();
+  Human ismail = SoftwareEngineer();
 }
+
 //Make a system inventory to manage a Mobile shop
+
+//Interface vs Implementation
+abstract class Human {
+  void eating ();
+  void moving();
+  void breathig();
+}
+class Student implements Human {
+  @override
+  void breathig() {
+    // TODO: implement breathig
+    print("Student is breathing.");
+  }
+
+  @override
+  void eating() {
+    // TODO: implement eating
+    print("Student is eating singara");
+  }
+
+  @override
+  void moving() {
+    // TODO: implement moving
+    print("Student is moving");
+  }
+}
+class SoftwareEngineer implements Human{
+  @override
+  void breathig() {
+    // TODO: implement breathig
+  }
+
+  @override
+  void eating() {
+    // TODO: implement eating
+  }
+
+  @override
+  void moving() {
+    // TODO: implement moving
+  }
+
+}
